7c8b3659bcb0ee4b5924923bed6bd952704e608b
VicG <vgumencuk@competo.io>
2023-03-06 19:16:02 +0300

    array-readonly

--- app.ts
+++ app.ts
@@ -1,6 +1,2 @@
-const skills:string[] = ['Dev', 'DevOps']
-for (const skill of skills){
-    console.log(skill);
-}
-const res = skills.filter((s:string)=>s!=='DevOps')
-console.log(res);
+const skill:readonly [number, string] = [1, 'DevOps']
+
