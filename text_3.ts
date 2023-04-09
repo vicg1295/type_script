db7be68c55170c2613a662dc072c676c06702f2f
vicg1295 <40528917+vicg1295@users.noreply.github.com>
2023-04-09 19:34:10 +0300

    void_27.ts

--- text_3.ts
+++ text_3.ts
@@ -1,35 +1,44 @@
 
-interface paymant {
+interface Paymant {
     sum: number,
     from: 2
     to: number
 }
 
-interface request extends paymant{}
+interface IPaymentRequest extends Paymant{}
 enum PaymentStatus {
     Success = 'success',
     Failed = 'failed'
 }
 
-interface IDataSuccess {
-    databaseId: number;
-    sun: number,
-    from: number,
-    to: number,
+// interface IDataSuccess {
+// status: string,
+//     databaseId: number;
+//     sun: number,
+//     from: number,
+//     to: number,
+// }
+// interface IDataFailed {
+//     status: string,
+//     data:{
+//         errorMessage: string,
+//         errorCode: 4,
+//     }
+// }
+// interface IResponse {
+//     status: PaymentStatus,
+//     data: IDataSuccess | IDataFailed
+// }
+
+
+
+interface IDataSuccess extends Paymant{
+    status: PaymentStatus.Success,
 }
 interface IDataFailed {
-    status: string,
+    status: PaymentStatus.Failed,
     data:{
         errorMessage: string,
         errorCode: 4,
     }
-}
-interface response {
-    status: PaymentStatus,
-    data: IDataSuccess | IDataFailed
-}
-
-
-function fetch(request:request): response {
-    return {}
 }
