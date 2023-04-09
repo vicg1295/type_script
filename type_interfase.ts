15a8a00e3acc3897f408f8c2eac7e9af99edc473
VicG <vgumencuk@competo.io>
2023-04-09 14:27:40 +0300

    typw interfase 24.ts

---
+++ type_interfase.ts
@@ -0,0 +1,33 @@
+export {}
+interface User{
+    name: string
+
+}
+
+interface User {
+    age: number
+}
+
+const user: User = {
+    name: 'as',
+    age: 33
+}
+console.log(user);
+
+
+
+
+type User_1 = {
+    name: string
+
+}
+
+type User_1  = {
+    age: number
+}
+
+const use_1: User_1 = {
+    name: 'as',
+    age: 33
+}
+console.log(user);
