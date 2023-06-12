4bc5dd3a9edac43ec82940d1341c02031cfe9653
vicg1295 <40528917+vicg1295@users.noreply.github.com>
2023-06-12 10:47:36 +0300

    59_ts generics

---
+++ Generics/59_example_generics.ts
@@ -0,0 +1,12 @@
+const num: Array<number> = [1,2,3]
+
+async function test(){
+    const a = new Promise<number>((resolve, reject) => {
+        resolve(1)
+    })
+}
+
+const check:Record<string, boolean> = {
+    drive: true,
+    kpp: false,
+}
